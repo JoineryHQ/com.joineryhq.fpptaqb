1dd083695cc30f594c9b9841bcef54c36cddb036
Allen Shaw <allen@JoineryHQ.com>
2022-11-17 10:54:26 -0600

    Correct CRM_Contribute_Form_AdditionalPayment javascript so it inserts fields at correct location.

--- js/CRM_Contribute_Form_AdditionalPayment.js
+++ js/CRM_Contribute_Form_AdditionalPayment.js
@@ -13,7 +13,7 @@ CRM.$(function($) {
   $('input#fpptaqb_is_creditmemo').closest('table').attr('id', 'bhfe_table');
 
   // Move all bhfe table rows into the main table after 'from_email_address'
-  var tr = $('#from_email_address').closest('tr');
+  var tr = $('input#total_amount').closest('table').find('tr:last-child');
   $('table#bhfe_table tr').insertAfter(tr);
   
   // Append desriptions after fields.
